b2ba2226dcfa312788be93b5c80bb8d61321e1f3
André Gonçalves da Silva <andre.godasi@gmail.com>
2017-07-30 14:21:17 -0300

    capitulo 3 finalizado

--- casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
+++ casadocodigo/src/main/java/br/com/casadocodigo/loja/daos/ProdutoDAO.java
@@ -1,13 +1,11 @@
 package br.com.casadocodigo.loja.daos;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceContext;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.com.casadocodigo.loja.models.Produto;
@@ -22,5 +20,9 @@ public class ProdutoDAO  {
 	public void gravar(Produto produto) {
 		
 		this.manager.persist(produto);
+	}
+	
+	public List<Produto> listar(){
+		return manager.createQuery("select p from Produto p",Produto.class).getResultList();
 	}
 }
